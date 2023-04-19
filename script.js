6c5841b6827cd9bee59be65f2ae7da617decca3c
Supercrazysquid <supercrazysquid@gmail.com>
2023-04-19 13:03:44 -0400

    Added authentication

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ let input = "";
 let canWrite = true;
 let writingBox = false; // Blinking symbol at the end of input
 
+let logging_in = true;
+
+let login_prompt = 0;
+let ip, user, pass;
+
 let blinking_symbol = "_";
 
 let filesystem = new trFolder("", [
@@ -24,151 +29,12 @@ const socket = io("http://localhost:3000", {
 });
 
 // Functions
-function stdout(out) {
-	outElement.innerText += out;
-
-	return out;
-}
-
-function updateInput() {
-	prompt = "guest@road:" + working_directory + "$ ";
-
-	if(writingBox) {
-		inElement.innerText = prompt + input + blinking_symbol;
-	} else {
-		inElement.innerText = prompt + input;
-	}
-}
-
-function findFilesInDir(folder_path) {
-	let directory = folder_path.split("/");
-
-	let files = filesystem;
-
-	for(let i = 0; i < directory.length; i++) {
-		let dir = directory[i];
-
-		if(dir !== "") {
-			for(let x = 0; x < files.contents.length; x++) {
-				let folder = files.contents[x];
-
-				if(folder != undefined && folder.constructor == trFolder && folder.name == dir) {
-					files = folder;
-
-					break;
-				}
-			}
-		}
-	}
-
-	return files;
-}
-
-function addFileToDir(folder_path, file) {
-	let directory = folder_path.split("/");
-
-	let files = filesystem;
-
-	for(let i = 0; i < directory.length; i++) {
-		let dir = directory[i];
-
-		if(dir !== "") {
-			for(let x = 0; x < files.contents.length; x++) {
-				let folder = files.contents[x];
-
-				if(folder != undefined && folder.constructor == trFolder && folder.name == dir) {
-					files = folder;
-
-					break;
-				}
-			}
-		}
-	}
-
-	files.contents.push(file);
-}
-
-function writeToFile(file_path, content) {
-	let split_path = file_path.split("/");
-
-	let file_name = split_path[split_path.length - 1];
-	
-	split_path.pop(); // Gets only the directories without the file
-
-	let files = filesystem;
-
-	for(let i = 0; i < split_path.length; i++) {
-		let dir = split_path[i];
-
-		if(dir !== "") {
-			for(let x = 0; x < files.contents.length; x++) {
-				let folder = files.contents[x];
-
-				if(folder != undefined && folder.constructor == trFolder && folder.name == dir) {
-					files = folder;
-
-					break;
-				}
-			}
-		}
-	}
-
-	let file;
-
-	for(let i = 0; i < files.contents.length; i++) {
-		let file_ = files.contents[i];
-
-		if(file_.name == file_name) {
-			file = file_;
-
-			break;
-		}
-	}
-
-	if(file == undefined) {
-		return -1;
-	} else {
-		file.content = content;
-	}
-}
-
-function delFile(file_path) {
-	let split_path = file_path.split("/");
-
-	let directory = split_path.splice(0, -1);
-	let file_name = split_path[-1];
-
-	let files = filesystem;
-
-	for(let i = 0; i < directory.length; i++) {
-		let dir = directory[i];
-
-		if(dir !== "") {
-			for(let x = 0; x < files.contents.length; x++) {
-				let folder = files.contents[x];
-
-				if(folder != undefined && folder.constructor == trFolder && folder.name == dir) {
-					files = folder;
-				
-					break;
-				}
-			}
-		}
-	}
-
-	let file_index = -1;
-
-	for(let i = 0; i < files.contents.length; i++) {
-		let file = files.contents[i];
-
-		if(file.name == file_name) {
-			file_index = i;
-
-			break;
-		}
-	}
-
-	files = files.contents.splice(file_index, 1);
+function signin(ip, username, password) {
+	socket.emit("login", {
+		"ip": ip,
+		"username": username,
+		"password": password
+	});
 }
 
 // Startup Code
@@ -190,26 +56,40 @@ document.addEventListener("keydown", (e) => {
 				case "Enter": // Running commands
 					stdout(prompt + input + "\n");
 
-					let split = input.split(" ");
-					let command = split[0];
+					if(!logging_in) {
+						let split = input.split(" ");
+						let command = split[0];
 
-					if(commands[command] != undefined) {
-						interpret(input);
-					} else {
-						let files = findFilesInDir("/bin");
+						if(commands[command] != undefined) {
+							interpret(input);
+						} else {
+							let files = findFilesInDir("/bin");
 
-						let content;
+							let content;
 
-						for(let i = 0; i < files.contents.length; i++) {
-							if(command === files.contents[i].name) {
-								content = files.contents[i].content;
+							for(let i = 0; i < files.contents.length; i++) {
+								if(command === files.contents[i].name) {
+									content = files.contents[i].content;
+								}
+							}
+
+							if(content == undefined) {
+								stdout("Illegal Command\n");
+							} else {
+								interpret(content);
 							}
 						}
+					} else {
+						login_prompt++;
 
-						if(content == undefined) {
-							stdout("Illegal Command\n");
-						} else {
-							interpret(content);
+						if(login_prompt == 3) {
+							signin(ip, user, pass);
+
+							login_prompt = -1;
+
+							canWrite = false;
+
+							updateInput();
 						}
 					}
 
@@ -230,6 +110,19 @@ document.addEventListener("keydown", (e) => {
 		updateInput();
 	}
 });
+
+// Socket events
+socket.on("signed_in", (data) => {
+	let signed_in = data['successful'];
+
+	logging_in = !signed_in;
+	canWrite = true;
+	login_prompt = 0;
+
+	if(!signed_in) {
+		stdout("Invalid Login...\n");
+	}
+})
 
 // Intervals
 setInterval(() => {
---
+++ utils.js
@@ -0,0 +1,175 @@
+function stdout(out) {
+	outElement.innerText += out;
+
+	return out;
+}
+
+function updateInput() {
+	if(logging_in) {
+		switch(login_prompt) {
+			case -1:
+				prompt = "";
+
+			case 0:
+				prompt = "IP: ";
+				ip = input;
+				break;
+
+			case 1:
+				prompt = "Username: ";
+				user = input;
+				break;
+
+			case 2:
+				prompt = "Password: ";
+				pass = input;
+				break;
+
+			case 3:
+				let signed_in = signin(ip, user, pass);
+
+				login_prompt = 0;
+
+				updateInput();
+		}
+	} else {
+		prompt = "guest@road:" + working_directory + "$ ";
+	}
+
+	if(writingBox) {
+		inElement.innerText = prompt + input + blinking_symbol;
+	} else {
+		inElement.innerText = prompt + input;
+	}
+}
+
+function findFilesInDir(folder_path) {
+	let directory = folder_path.split("/");
+
+	let files = filesystem;
+
+	for(let i = 0; i < directory.length; i++) {
+		let dir = directory[i];
+
+		if(dir !== "") {
+			for(let x = 0; x < files.contents.length; x++) {
+				let folder = files.contents[x];
+
+				if(folder != undefined && folder.constructor == trFolder && folder.name == dir) {
+					files = folder;
+
+					break;
+				}
+			}
+		}
+	}
+
+	return files;
+}
+
+function addFileToDir(folder_path, file) {
+	let directory = folder_path.split("/");
+
+	let files = filesystem;
+
+	for(let i = 0; i < directory.length; i++) {
+		let dir = directory[i];
+
+		if(dir !== "") {
+			for(let x = 0; x < files.contents.length; x++) {
+				let folder = files.contents[x];
+
+				if(folder != undefined && folder.constructor == trFolder && folder.name == dir) {
+					files = folder;
+
+					break;
+				}
+			}
+		}
+	}
+
+	files.contents.push(file);
+}
+
+function writeToFile(file_path, content) {
+	let split_path = file_path.split("/");
+
+	let file_name = split_path[split_path.length - 1];
+	
+	split_path.pop(); // Gets only the directories without the file
+
+	let files = filesystem;
+
+	for(let i = 0; i < split_path.length; i++) {
+		let dir = split_path[i];
+
+		if(dir !== "") {
+			for(let x = 0; x < files.contents.length; x++) {
+				let folder = files.contents[x];
+
+				if(folder != undefined && folder.constructor == trFolder && folder.name == dir) {
+					files = folder;
+
+					break;
+				}
+			}
+		}
+	}
+
+	let file;
+
+	for(let i = 0; i < files.contents.length; i++) {
+		let file_ = files.contents[i];
+
+		if(file_.name == file_name) {
+			file = file_;
+
+			break;
+		}
+	}
+
+	if(file == undefined) {
+		return -1;
+	} else {
+		file.content = content;
+	}
+}
+
+function delFile(file_path) {
+	let split_path = file_path.split("/");
+
+	let directory = split_path.splice(0, -1);
+	let file_name = split_path[-1];
+
+	let files = filesystem;
+
+	for(let i = 0; i < directory.length; i++) {
+		let dir = directory[i];
+
+		if(dir !== "") {
+			for(let x = 0; x < files.contents.length; x++) {
+				let folder = files.contents[x];
+
+				if(folder != undefined && folder.constructor == trFolder && folder.name == dir) {
+					files = folder;
+				
+					break;
+				}
+			}
+		}
+	}
+
+	let file_index = -1;
+
+	for(let i = 0; i < files.contents.length; i++) {
+		let file = files.contents[i];
+
+		if(file.name == file_name) {
+			file_index = i;
+
+			break;
+		}
+	}
+
+	files = files.contents.splice(file_index, 1);
+}
